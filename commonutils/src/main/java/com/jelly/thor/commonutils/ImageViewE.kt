1d0aa081604d2c68199123e4457433cf0cfcc443
dongdong.wu <withwudongdong@gmail.com>
2020-03-31 19:15:09 +0800

    代码优化

--- commonutils/src/main/java/com/jelly/thor/commonutils/ImageViewE.kt
+++ commonutils/src/main/java/com/jelly/thor/commonutils/ImageViewE.kt
@@ -9,24 +9,28 @@ import androidx.annotation.DrawableRes
  * 创建时间：2019/7/17 10:29 <br/>
  */
 //@JvmOverloads
-//fun ImageView.glideIntoAsBitmapPath(path: String?,
+//fun ImageView.glideIntoAsBitmapPath(path: Any?,
 //                                    @DrawableRes loadingPic: Int = R.drawable.ic_circular_picture_loading,
 //                                    @DrawableRes errorPic: Int = R.drawable.ic_circular_picture_error,
 //                                    @DrawableRes noPic: Int = R.drawable.ic_circular_picture_no
 //) {
 //    val requestOptions = RequestOptions()
-//            .placeholder(loadingPic)
-//            .error(errorPic)
-//            .fallback(errorPic)
+//        .placeholder(loadingPic)
+//        .error(errorPic)
+//        .fallback(errorPic)
 //
 //    Glide.with(BaseApplication.getInstance().applicationContext)
-//            .asBitmap()
-//            .apply(requestOptions)
-//            .load(
-//                    if (path.isNullOrEmpty()) {
-//                        noPic
-//                    } else {
-//                        path
-//                    }
-//            ).into(this)
+//        .asBitmap()
+//        .apply(requestOptions)
+//        .load(
+//            if (path == null) {
+//                noPic
+//            } else if (path is String
+//                && path.isEmpty()
+//            ) {
+//                noPic
+//            } else {
+//                path
+//            }
+//        ).into(this)
 //}
